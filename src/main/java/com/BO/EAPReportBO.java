ace6e434c2da54aad9f36c27e9f6778ba9458405
MitchellUngar <mitchell.ungar@bbd.ca>
2019-03-04 15:07:08 -0500

    SUM-8439: Creating HumanaCare EAP Report and converting Arete EAP Report to DJ

---
+++ src/main/java/com/BO/EAPReportBO.java
@@ -0,0 +1,32 @@
+package com.BO;
+
+import com.Component.ME.EAPReport.container.EapGroup;
+import com.DAO.EAPReport.EAPReportDAO;
+import com.DAO.EAPReport.EAPReportDAOImpl;
+import com.DemoDynamicJasper.spring.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public class EAPReportBO
+{
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class );
+    private static final Logger LOGGER = Logger.getLogger(EAPReportBO.class.getName() );
+    EAPReportDAO eapReportDAO = context.getBean(EAPReportDAOImpl.class );
+
+    public ArrayList<EapGroup> getEAPGroups(int underwriterId)
+    {
+        String insurer;
+        if (underwriterId == 93064)
+        {
+            insurer = "Arete";
+        }
+        else
+        {
+            insurer = "HumanaCare";
+        }
+
+        return eapReportDAO.getGroupList(underwriterId, insurer);
+    }
+}
